d214fe472f59367b0f8fa88f5540a5b5a5e07d2b
pulloutthephone <qualquertreta@gmail.com>
2024-12-09 18:54:11 +0000

    Identation changes

--- src/matlab/CommunicationTest.m
+++ src/matlab/CommunicationTest.m
@@ -1,23 +1,23 @@
-clear;                                          % Limpa variáveis do workspace
-clc;                                            % Limpa o Command Window
+clear;                                                      % Limpa variáveis do workspace
+clc;                                                        % Limpa o Command Window
 
 % Configuração da porta serial
-arduinoPort = "COM7";                           % Porta COM usada pelo Arduino
-baudRate = 115200;                              % Taxa de comunicação serial (bps)
+arduinoPort = "COM7";                                       % Porta COM usada pelo Arduino
+baudRate    = 115200;                                       % Taxa de comunicação serial (bps)
 
 % Inicializa a porta serial se ainda não estiver configurada
 if ~exist('porto', 'var')
-    porto = serialport(arduinoPort, baudRate);  % Configura a porta serial
-    configureTerminator(porto, "CR/LF");        % Define terminador de linha
-    porto.Timeout = 30;                         % Define tempo limite de espera
+    porto           = serialport(arduinoPort, baudRate);    % Configura a porta serial
+    porto.Timeout   = 30;                                   % Define tempo limite de espera
+    configureTerminator(porto, "CR/LF");                    % Define terminador de linha
 end
-pause(10);                                   % Aguarda inicialização do Arduino
+pause(10);                                                  % Aguarda inicialização do Arduino
 disp("Solicitando medições para 360°...");
 
 % Inicializa a matriz de resultados
 numAmostrasPorGrau  = 10;
-valores             = zeros(360, 1);                % Vetor para armazenar as medições totais
-valoresDoGrau       = zeros(numAmostrasPorGrau, 1); % Vetor para armazenar as medições de um grau
+valores             = zeros(360, 1);                        % Vetor para armazenar as medições totais
+valoresDoGrau       = zeros(numAmostrasPorGrau, 1);         % Vetor para armazenar as medições de um grau
 
 for i=1:360
     try
@@ -32,6 +32,6 @@ for i=1:360
 end
 
 disp("Vetor de medições coletadas:");
-disp(valores);                                                      % Mostra a matriz coletada
+disp(valores);                                              % Mostra a o vetor coletado
 
-clear porto;                                                        % Fecha e limpa a configuração da porta serial
+clear porto;                                                % Fecha e limpa a configuração da porta serial
