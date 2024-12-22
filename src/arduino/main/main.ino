e2f5c1eee74000d789f2e34d2e77e1f872498ff6
pulloutthephone <qualquertreta@gmail.com>
2024-12-22 19:35:49 +0000

    Removed 'velocity control' since it wasn't necessary in the first place

--- src/arduino/main/main.ino
+++ src/arduino/main/main.ino
@@ -30,8 +30,8 @@
 #include <Arduino.h>
 
 
-enum motor_direction  { RIGHT = LOW, LEFT = HIGH };
-enum system_state     { LISTEN = 0, PROCESS = 1 };
+enum motor_direction    { RIGHT     = LOW   , LEFT      = HIGH };
+enum system_state       { LISTEN    = 0     , PROCESS   = 1    };
 
 // Port definitions
 #define                 M1_PORT             PORTH
@@ -61,21 +61,19 @@ const uint8_t           HALL_PIN            = A13;  // Hall sensor
 const uint8_t           RF_PIN              = A15;  // Radiofrequency module
 
 // Constants definitions
-const uint8_t           MICROSTEPS_TO_DEG   = 16;
 const int16_t           MAX_ANGLE           = 179;
 const int16_t           MIN_ANGLE           = -180;
+const uint8_t           MICROSTEPS_TO_DEG   = 16;
 const size_t            SAMPLES             = 10;
-const double            DEFAULT_VELOCITY    = 1.09; // Angular velocity (rad/s)
 const motor_direction   DEFAULT_DIRECTION   = RIGHT;
-const uint32_t          MOTOR_SLEEP_TIMEOUT = 10000000;
 
 // Function prototypes
 bool home_motor_to_origin();
 void capture_sensor_data(uint16_t *sensor_values, size_t samples);
 void rotate_motor_to_next_sample();
-void rotate_motor_step(const motor_direction direction);
+void inline rotate_motor_step(const motor_direction direction);
 void transmit_sensor_data(uint16_t *sensor_values, size_t samples);
-void sleep_motor(uint32_t last_active); 
+void inline sleep_motor(const uint32_t last_active); 
 
 void setup()
 {
@@ -129,7 +127,7 @@ void loop()
     }
     else if (mode == PROCESS)
     {
-        uint16_t sensor_values[SAMPLES]  = {0};
+        uint16_t sensor_values[SAMPLES]  = { 0 };
         
         ENABLE_MOTOR();
 
@@ -168,9 +166,9 @@ bool home_motor_to_origin()
      *      - Step 3  : Returning the motor to the center of this range.
      */
 
-    const uint16_t  THRESHOLD       = 50;
+    const uint16_t  HOMING_DELAY    = 10000;    // Lowers the velocity
+    const uint16_t  HALL_THRESHOLD  = 50;       
     const uint16_t  MAX_STEPS       = 3200;     // 16 * 200
-    const double    HOMING_VELOCITY = 0.109;    // Slow velocity for precision
     uint16_t        steps_completed = 0;
     int16_t         start_step      = -1;
     int16_t         end_step        = -1;
@@ -179,24 +177,25 @@ bool home_motor_to_origin()
     // rotates backwards until it doesn't detect it anymore
     while (analogRead(HALL_PIN) < THRESHOLD) 
     { 
-        rotate_motor_step((motor_direction)(!DEFAULT_DIRECTION),
-                          DEFAULT_VELOCITY); 
+        rotate_motor_step((motor_direction)(!DEFAULT_DIRECTION)); 
     }
 
     while (steps_completed < MAX_STEPS)
     {
-        uint16_t sensor_value = analogRead(HALL_PIN);
+        uint16_t hall_value = analogRead(HALL_PIN);
 
-        if (sensor_value < THRESHOLD && start_step == -1)
+        if (hall_value < HALL_THRESHOLD && start_step == -1)
         {
             start_step = steps_completed;
         }
-        else if (sensor_value >= THRESHOLD && start_step != -1)
+        else if (hall_value >= HALL_THRESHOLD && start_step != -1)
         {
             end_step = steps_completed;
             break;
         }
-        rotate_motor_step(DEFAULT_DIRECTION, HOMING_VELOCITY);
+        rotate_motor_step(DEFAULT_DIRECTION);
+        delayMicroseconds(HOMING_DELAY);
+
         steps_completed++;
     }
 
@@ -209,8 +208,8 @@ bool home_motor_to_origin()
     for (uint16_t central_step = (end_step - start_step) / 2; central_step > 0;
          central_step--)
     {
-        rotate_motor_step((motor_direction)(!DEFAULT_DIRECTION),
-                          DEFAULT_VELOCITY);
+        rotate_motor_step((motor_direction)(!DEFAULT_DIRECTION));
+        delayMicroseconds(HOMING_DELAY);
     }
 
     Serial.println("Motor homed.");
@@ -253,21 +252,23 @@ void rotate_motor_to_next_sample()
     // Rotate the motor by 1° in the current direction
     for (size_t i = 0; i < MICROSTEPS_TO_DEG; i++)
     {
-        rotate_motor_step(direction, DEFAULT_VELOCITY);
+        rotate_motor_step(direction);
     }
     current_angle += (direction == DEFAULT_DIRECTION) ? 1 : -1;
 }
 
-void rotate_motor_step(const motor_direction direction, const double velocity)
+void inline rotate_motor_step(const motor_direction direction)
 {
-    // Calculate the wait time in microseconds for an input velocity in rad/s
-    const double wait_time = (PI * 1e6) / (velocity * 180 * MICROSTEPS_TO_DEG);
+    static const uint32_t   MIN_STEP_INTERVAL   = 100;
+    static uint32_t         last_step_time      = 0;
+
+    // Make sure the stepper motor has stabilized
+    while ((uint32_t)(micros() - last_step_time) < MIN_STEP_INTERVAL);
 
     SET_DIR(direction);
     STEP();
 
-    // Delay to control motor speed
-    delayMicroseconds(wait_time);
+    last_step_time = micros();
 }
 
 void transmit_sensor_data(uint16_t *sensor_values, size_t samples)
@@ -293,9 +294,11 @@ void transmit_sensor_data(uint16_t *sensor_values, size_t samples)
     Serial.println(buffer);
 }
 
-void sleep_motor(const uint32_t last_active)
+void inline sleep_motor(const uint32_t last_active)
 {
-    if (micros() - last_active >= MOTOR_SLEEP_TIMEOUT)
+    static const uint32_t MOTOR_SLEEP_TIMEOUT = 10000000;
+    
+    if ((uint32_t)(micros() - last_active) >= MOTOR_SLEEP_TIMEOUT)
     {
         DISABLE_MOTOR();
     }
